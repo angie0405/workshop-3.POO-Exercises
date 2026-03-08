56b21680c60dfc4474457f4a7ac3c4021dcdaed2
Angie Manuela Marin <Angie Manuela Marin@ANG-2021>
2026-03-08 15:18:48 -0500

    exercise 31 complet

---
+++ Exercise 31/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Ingrese el orden de la matriz: ");
+        int n = int.Parse(Console.ReadLine());
+
+        // create matrix
+        int[,] matrix = new int[n, n];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = i + j;
+
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        // Lower triangular matrix
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
