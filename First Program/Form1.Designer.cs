583b8cda0504e24e714e083a929eb116f4cefe6c
Angie Manuela Marin <Angie Manuela Marin@ANG-2021>
2026-05-06 18:07:48 -0500

    First Program

---
+++ First Program/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace First_Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnVerMensaje = new Button();
+            SuspendLayout();
+            // 
+            // btnVerMensaje
+            // 
+            btnVerMensaje.Location = new Point(88, 154);
+            btnVerMensaje.Name = "btnVerMensaje";
+            btnVerMensaje.Size = new Size(185, 55);
+            btnVerMensaje.TabIndex = 0;
+            btnVerMensaje.Text = "VER MENSAJE";
+            btnVerMensaje.UseVisualStyleBackColor = true;
+            btnVerMensaje.Click += btnVerMensaje_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(364, 245);
+            Controls.Add(btnVerMensaje);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnVerMensaje;
+    }
+}
---
+++ First Program/Form1.cs
@@ -0,0 +1,15 @@
+namespace First_Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerMensaje_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola, este es mi primer programa en C#");
+        }
+    }
+}
